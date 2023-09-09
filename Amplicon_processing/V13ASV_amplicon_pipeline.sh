51b92172b1abc544239565d78ae5dc11cc82f4d8
Morten Simonsen Dueholm <md@bio.aau.dk>
2023-09-09 15:55:04 +0200

    Data added

---
+++ Amplicon_processing/V13ASV_amplicon_pipeline.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Retrive, merge, and relabel V13 forward and reverse reads based in a sample list (samples.txt) and merge them into one file.
+
+mkdir V13_raw_data
+
+while read SAMPLES
+do
+NAME=$SAMPLES;
+find /raw_data/sequences/Illumina/MiSeq/ -name $NAME\_*R1* -exec gzip -cd {} \; > V13_raw_data/$NAME\_R1.fq 
+find /raw_data/sequences/Illumina/MiSeq/ -name $NAME\_*R2* -exec gzip -cd {} \; > V13_raw_data/$NAME\_R2.fq 
+usearch11 -fastq_mergepairs V13_raw_data/$NAME\_R1.fq -sample @ -fastqout V13_raw_data/temp.$NAME.fq
+usearch11 -fastx_relabel V13_raw_data/temp.$NAME.fq -prefix $NAME\_ -fastqout V13_raw_data/temp2.$NAME.fq
+cat V13_raw_data/temp2.$NAME.fq >> V13_raw_data/V13_merged.fastq
+done < V13_samples.txt
+date
+
+rm V13_raw_data/temp*.fq
+
+# Usearch pipeline
+
+mkdir V13_Usearch_analysis
+
+### Quality filtering
+usearch11 -filter_phix V13_raw_data/V13_merged.fastq -output V13_Usearch_analysis/V13_phixfiltered.fq
+usearch11 -fastq_filter V13_Usearch_analysis/V13_phixfiltered.fq -fastq_maxee 1.0 -fastaout V13_Usearch_analysis/V13_qcfiltered.fa
+
+### Identify unique sequences
+usearch11 -fastx_uniques V13_Usearch_analysis/V13_qcfiltered.fa -fastaout V13_Usearch_analysis/V13_unique.fa -sizeout -relabel uniq
+
+### Create ASVs
+usearch11 -unoise3 V13_Usearch_analysis/V13_unique.fa -zotus V13_Usearch_analysis/V13_ASV.fa
+sed -i 's/Zotu/ASV/g' V13_Usearch_analysis/V13_ASV.fa 
+
+### Create ASV table
+usearch11 -otutab V13_Usearch_analysis/V13_qcfiltered.fa -zotus V13_Usearch_analysis/V13_ASV.fa -sample_delim _\
+     -strand plus -otutabout V13_Usearch_analysis/V13_ASVtab.txt -threads 10
+
+### Predict taxonomy using sintax (MiDAS5.1)
+usearch11 -sintax V13_Usearch_analysis/V13_ASV.fa -db /user_data/md/Projects/P026/MiDAS5/MiDAS5.1_20230726/output/FLASVs_w_sintax.fa\
+     -tabbedout V13_Usearch_analysis/V13_ASV_midas5.1.sintax -strand plus -sintax_cutoff 0.8 -threads 10
---
+++ Amplicon_processing/V4ASV_amplicon_pipeline.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# Retrive and relabel V4 forward reads based on a sample list (samples.txt) and merge them into one file.
+
+mkdir V4_raw_data
+
+while read SAMPLES
+do
+NAME=$SAMPLES;
+find /raw_data/sequences/Illumina/MiSeq/ -name $NAME\_*R1* -exec gzip -cd {} \; > V4_raw_data/$NAME.R1.fq 
+usearch11 -fastx_relabel V4_raw_data/$NAME.R1.fq -prefix $NAME\_ -fastqout V4_raw_data/temp.$NAME.R1.fq
+cat V4_raw_data/temp.$NAME.R1.fq >> V4_raw_data/V4_forward.fastq
+done < V4_samples.txt
+date
+
+rm V4_raw_data/temp*.fq
+
+# Usearch pipeline
+mkdir V4_Usearch_analysis
+
+### Adapter trimming using cutadapt as the exact position of the primers are not known
+module load cutadapt/2.8-foss-2018a-Python-3.6.4 # calling in-house installed version of cutadapt
+cutadapt -g ^GGACTACHVGGGTWTCTAAT...TTACCGCGGCKGCTGGCAC -o V4_Usearch_analysis/V4_trimmed.fq --discard-untrimmed V4_raw_data/V4_forward.fastq -j 10
+
+### Quality filtering
+usearch11 -fastx_revcomp V4_Usearch_analysis/V4_trimmed.fq -fastqout V4_Usearch_analysis/V4_trimmed_rc.fq -threads 10 #reverse complement as our V4 reads are read in reverse
+usearch11 -fastq_filter V4_Usearch_analysis/V4_trimmed_rc.fq -fastq_maxee 1.0 -fastaout V4_Usearch_analysis/V4_qcfiltered.fa -threads 10
+
+### Identify unique sequences
+usearch11 -fastx_uniques V4_Usearch_analysis/V4_qcfiltered.fa -fastaout V4_Usearch_analysis/V4_unique.fa -sizeout -relabel uniq
+
+### Create ASVs
+usearch11 -unoise3 V4_Usearch_analysis/V4_unique.fa -zotus V4_Usearch_analysis/V4_ASV.fa
+sed -i 's/Zotu/ASV/g' V4_Usearch_analysis/V4_ASV.fa 
+
+### Create ASV table
+usearch11 -otutab V4_Usearch_analysis/V4_qcfiltered.fa -zotus V4_Usearch_analysis/V4_ASV.fa -sample_delim _ -strand plus\
+     -otutabout V4_Usearch_analysis/V4_ASVtab.txt -threads 10
+
+### Predict taxonomy using sintax (MiDAS5.1)
+usearch11 -sintax V4_Usearch_analysis/V4_ASV.fa -db /user_data/md/Projects/P026/MiDAS5/MiDAS5.1_20230726/output/FLASVs_w_sintax.fa\
+     -tabbedout V4_Usearch_analysis/V4_ASV_MiDAS_5.1.sintax -strand plus -sintax_cutoff 0.8 -threads 10
